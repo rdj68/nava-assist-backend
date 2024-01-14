b4f40619a87acd067416909ea1acc11e4c3b63a7
Rajeev Singh <rsinghh68@gmail.com>
2024-01-14 23:27:40 +0530

    Build.sh bug fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 python3 -m venv .venv
 
 # Activate the virtual environment
-source venv/bin/activate
+source .venv/bin/activate
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
