6358e78298ce7a801e41714bd06439b9cb4ecd32
Rajeev Singh <rsinghh68@gmail.com>
2024-01-18 18:08:34 +0530

    Removed the params passed when clients are initiated as it is not needed since ADC handles authentication

--- aiModelClient/Clients.py
+++ aiModelClient/Clients.py
@@ -1,16 +1,7 @@
 import vertexai
 from vertexai.language_models import CodeGenerationModel, CodeChatModel
 from vertexai.preview.generative_models import GenerativeModel
-import google.generativeai as genai
-from google.oauth2 import service_account
-import os
 
-class Config:
-    def __init__(self):
-        self.project_id = os.environ.get('GOOGLE_CLOUD_PROJECT_ID')
-        self.location = os.environ.get('GOOGLE_CLOUD_LOCATION')
-        file_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-        self.credentials = service_account.Credentials.from_service_account_file(file_path)
 
 class GeckoClient:
     def __init__(self):
@@ -23,20 +14,18 @@ class GeckoClient:
             "max_output_tokens": 64,
             "temperature": 0.9
         }
-        response = await self.model.predict_async(prefix, suffix = suffix, **parameters)
+        response = await self.model.predict_async(prefix, suffix=suffix, **parameters)
         return response
+
 
 class BisonClient:
     def __init__(self):
-        self.config = Config()
-        vertexai.init(project=self.config.project_id,
-                      location=self.config.location, 
-                      credentials=self.config.credentials)
+        vertexai.init()
         self.model = CodeChatModel.from_pretrained("codechat-bison@001")
         self.chat = self.model.start_chat(
             context="You are Nava Assist, an AI assistant for developers.You can write, explain and generate code and answer any user queries."
         )
-        
+
     async def prompt(self, promptMessage):
         parameters = {
             "temperature": 0.5,
@@ -45,26 +34,24 @@ class BisonClient:
         response = await self.chat.send_message_async(promptMessage, **parameters)
         return response
 
+
 class GeminiClient:
     def __init__(self):
-        self.config = Config()
-        vertexai.init(project=self.config.project_id,
-                      location=self.config.location, 
-                      credentials=self.config.credentials)
+        vertexai.init()
         self.model = GenerativeModel("gemini-pro")
         self.chat = self.model.start_chat()
-        
+
     async def prompt(self, promptMessage):
         response = await self.chat.send_message_async(promptMessage, stream=True)
         return response
 
-class GeminiClientGenAi:
-    def __init__(self):
-        GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
-        genai.configure(api_key=GOOGLE_API_KEY)
-        self.model = genai.GenerativeModel('gemini-pro')
-        self.chat = self.model.start_chat()
-        
-    def prompt(self, promptMessage):
-        response = self.chat.send_message(promptMessage, stream=True)
-        return response
+# class GeminiClientGenAi:
+#     def __init__(self):
+#         GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+#         genai.configure(api_key=GOOGLE_API_KEY)
+#         self.model = genai.GenerativeModel('gemini-pro')
+#         self.chat = self.model.start_chat()
+
+#     def prompt(self, promptMessage):
+#         response = self.chat.send_message(promptMessage, stream=True)
+#         return response
