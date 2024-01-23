3c9e87069d2b71f15461152088b5b8ce1fc02dd0
Rajeev Singh <rsinghh68@gmail.com>
2024-01-24 00:31:47 +0530

    fix

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ async def lifespan(app: FastAPI):
     """
     initialize crucial application services 
     """
-    os.environ.clear()
+    # os.environ.clear()
     load_dotenv(".env")
     db_client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
     vertexai.init()
