93d63e762bcc584eb30c33b29720f50873488067
Rajeev Singh <rsinghh68@gmail.com>
2024-01-29 09:10:49 +0530

    changed build config

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create a Python virtual environment
-python3 -m venv .venv
+python3 -m venv venv
 
 # Activate the virtual environment
-source .venv/bin/activate
+source venv/bin/activate
 
 # Install dependencies from requirements.txt
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
